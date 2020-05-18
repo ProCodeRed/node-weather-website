fefa1e195e171144150f0579d4456d4b1fdf2711
ProCodeRed <yadav.vipinkumar@outlook.com>
2020-05-18 14:32:08 +0530

    progress bar bug fixed

--- public/js/app.js
+++ public/js/app.js
@@ -183,8 +183,7 @@ weatherForm.addEventListener('submit', (e) => {
 
 
 // When the user scrolls the page, execute myFunction
-
-function myFunction() {
+window.onscroll =  function myFunction() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
